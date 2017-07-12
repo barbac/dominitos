57fd59c47593f96f0e1e0a51f947472a54d966a4
barbac <jjbarbac+git@gmail.com>
2017-07-12 16:51:32 -0500

    Remove /dist from urls

--- src/main.js
+++ src/main.js
@@ -3,6 +3,5 @@ import page from 'page';
 import dominitos from './dominitos.js';
 
 
-page.base('/dist');
 page('/', dominitos);
 page();
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
 
+const distDir = path.resolve(__dirname, 'dist');
+
 module.exports = {
   entry: './src/main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: distDir,
   },
   devServer: {
     port: 9000,
+    contentBase: distDir,
   },
   devtool: 'cheap-module-eval-source-map',
   module: {

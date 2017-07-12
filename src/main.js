391bb9d2f0778b111def72b496a11bcb6bf13828
barbac <jjbarbac+git@gmail.com>
2017-07-12 18:43:21 -0500

    Add vectors route

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import page from 'page';
 
 import dominitos from './dominitos.js';
-
+import vectors from './vectors.js';
 
 page('/', dominitos);
+page('/vectors.html', vectors);
 page();
---
+++ src/vectors.js
@@ -0,0 +1,4 @@
+function vectors() {
+}
+
+export default vectors;

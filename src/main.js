8e837da492ca4521a7fe4697064c3d8ef6f5e240
barbac <jjbarbac+git@gmail.com>
2018-03-22 13:28:15 -0600

    Add list of pages

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import page from 'page';
 import dominitos from './dominitos.js';
 import vectors from './vectors.js';
 
-page('/', dominitos);
+page('/dominitos.html', dominitos);
 page('/vectors.html', vectors);
 page();

28f98db1bbf52c7c9016a4eba55182385a607650
barbac <jjbarbac+git@gmail.com>
2018-03-28 15:45:30 -0600

    Add wasd movement for first person camera

--- src/worldView.js
+++ src/worldView.js
@@ -5,16 +5,44 @@ let scene, camera, cameraContainer, renderer;
 const planeSize = 130;
 const halfPlaneSize = planeSize / 2;
 const rotationFactor = 0.01;
+const cameraPanningDelta = 10;
 
 function firstPersonCamera(event) {
   camera.rotation.x -= event.movementY * rotationFactor;
   camera.rotation.y -= event.movementX * rotationFactor;
 }
 
+function wasdCameraMovement(event) {
+  const vector = new THREE.Vector3(0, 0, 0);
+  switch (event.key) {
+    case 'w':
+      vector.z -= cameraPanningDelta;
+      break;
+    case 'a':
+      vector.x -= cameraPanningDelta;
+      break;
+    case 's':
+      vector.z += cameraPanningDelta;
+      break;
+    case 'd':
+      vector.x += cameraPanningDelta;
+      break;
+    default:
+  }
+  //Use camera.rotation.x for up and down movements if needed later.
+  const euler = new THREE.Euler(0, camera.rotation.y, 0, camera.rotation.order);
+  vector.applyEuler(euler);
+  cameraContainer.position.add(vector);
+}
+
 function inputControls(domElement) {
   //this order makes it look like a fps game.
   camera.rotation.order = 'YXZ';
   domElement.addEventListener('mousemove', firstPersonCamera);
+
+  //allow keyword focus
+  domElement.tabIndex = 1;
+  domElement.addEventListener('keydown', wasdCameraMovement);
 }
 
 function init() {
@@ -29,6 +57,7 @@ function init() {
 
   //Use this to translate the camara and avoid messing with the rotation.
   cameraContainer = new THREE.Object3D();
+  cameraContainer.position.y = 10;
   cameraContainer.add(camera);
   scene.add(cameraContainer);
 

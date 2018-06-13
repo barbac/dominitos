3fed00457c66a383a2758cdde6b07500adb8262a
barbac <jjbarbac+git@gmail.com>
2018-06-13 00:24:34 -0500

    Add dummy gripper model

--- src/models.js
+++ src/models.js
@@ -1,9 +1,11 @@
 import box from './box.js';
 import claw from './claw.js';
 import arm from './arm.js';
+import gripper from './robots/gripper.js';
 
 export default {
   box,
   claw,
   arm,
+  gripper,
 };
---
+++ src/robots/gripper.js
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+
+function makeGripper() {
+  const width = 5.4;
+  const height = 10;
+  const thickness = 2;
+
+  const gripperModel = new THREE.Mesh(
+    new THREE.BoxGeometry(width, height, thickness),
+    new THREE.MeshBasicMaterial({ color: 0x46f3ff, wireframe: true })
+  );
+
+  const gripperMarcSize = thickness / 3;
+  const gripperMarc = new THREE.Mesh(
+    new THREE.BoxGeometry(gripperMarcSize, gripperMarcSize, gripperMarcSize),
+    new THREE.MeshBasicMaterial({ color: 'orange' })
+  );
+  gripperMarc.position.y = height / -2 + gripperMarcSize / 2;
+  gripperMarc.position.z = thickness - gripperMarcSize / 2;
+  gripperModel.add(gripperMarc);
+
+  return {
+    model: gripperModel,
+    dimensions: {
+      width,
+      height,
+      thickness,
+    },
+  };
+}
+
+export default makeGripper;

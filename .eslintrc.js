05ab542592ab2c7524bf919fb1d18b0a857999c4
barbac <jjbarbac+git@gmail.com>
2018-06-24 12:20:22 -0500

    Use prettier on .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 7,
-        "sourceType": "module"
-    },
-    "rules": {
-        "no-console": 0,
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 0,
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
 };

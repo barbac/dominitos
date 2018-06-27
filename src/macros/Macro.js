5492354d44da22b01e0bc21385713e5949a27126
barbac <jjbarbac+git@gmail.com>
2018-06-27 14:19:01 -0500

    Add Macro class

---
+++ src/macros/Macro.js
@@ -0,0 +1,36 @@
+export default class Macro {
+  name = '';
+  id = null;
+  _activeStep = 0;
+  steps = [new Map(), new Map()]; //2 empty steps by default.
+  onStepChanged = null;
+  controls = [];
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  registerControl(func) {
+    this.controls.push(func);
+  }
+
+  get activeStep() {
+    return this._activeStep;
+  }
+
+  set activeStep(index) {
+    this._activeStep = index;
+    for (const func of this.controls) {
+      func();
+    }
+  }
+
+  get(controlName) {
+    const stepValues = this.steps[this.activeStep];
+    return stepValues ? stepValues.get(controlName) : null;
+  }
+
+  set(controlName, value) {
+    this.steps[this.activeStep].set(controlName, value);
+  }
+}

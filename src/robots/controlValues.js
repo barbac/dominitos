9d71f34e5bf0d1b6fdc0ec8c4b4854f94a38e14d
barbac <jjbarbac+git@gmail.com>
2018-06-27 10:37:36 -0500

    Use a class for rotation control values

--- src/robots/controlValues.js
+++ src/robots/controlValues.js
@@ -1,34 +1,46 @@
 import utils from '../utils.js';
 
+class RotationControlValues {
+  name = '';
+  type = 'range';
+  min = 0;
+  max = 180;
+  axis = null;
+  object3d = null;
+  _degreesValue = 0;
+  setRadians = null;
+
+  constructor(object3d, name, axis, setRadians) {
+    this.object3d = object3d;
+    this.name = name;
+    this.axis = axis;
+    this.setRadians = setRadians;
+  }
+
+  set degrees(value) {
+    this._degreesValue = value;
+    this.radians = utils.radians(value);
+  }
+
+  get degrees() {
+    return this._degreesValue;
+  }
+
+  set radians(value) {
+    if (this.setRadians) {
+      this.setRadians(value);
+    } else {
+      this.object3d.rotation[this.axis] = value;
+    }
+  }
+
+  get radians() {
+    return this.object3d.rotation[this.axis];
+  }
+}
+
 function rotationControlValues(object3d, name, axis, setRadians) {
-  return [
-    name,
-    {
-      name,
-      type: 'range',
-      min: 0,
-      max: 180,
-      _degreesValue: 0,
-      set degrees(value) {
-        this._degreesValue = value;
-        this.radians = utils.radians(value);
-      },
-      get degrees() {
-        return this._degreesValue;
-      },
-      setRadians,
-      set radians(value) {
-        if (this.setRadians) {
-          this.setRadians(value);
-        } else {
-          object3d.rotation[axis] = value;
-        }
-      },
-      get radians() {
-        return object3d.rotation[axis];
-      },
-    },
-  ];
+  return [name, new RotationControlValues(object3d, name, axis, setRadians)];
 }
 
 export default rotationControlValues;

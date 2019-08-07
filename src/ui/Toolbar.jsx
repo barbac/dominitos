177a62330bfc77ec63135eb966511ffd6147b636
barbac <jjbarbac+git@gmail.com>
2019-08-06 21:44:12 -0500

    Add AWSD instructions

--- src/ui/Toolbar.jsx
+++ src/ui/Toolbar.jsx
@@ -11,15 +11,20 @@ class Toolbar extends React.Component {
     return (
       <div id="toolbar" style={style}>
         <div>
-          <label>Lock camera</label>
-          <input
-            type="checkbox"
-            checked={settings.lockCamera}
-            onChange={() => {
-              settings.lockCamera = !settings.lockCamera;
-              this.forceUpdate();
-            }}
-          />
+          <label>
+            Lock camera (press L)
+            <input
+              type="checkbox"
+              checked={settings.lockCamera}
+              onChange={() => {
+                settings.lockCamera = !settings.lockCamera;
+                this.forceUpdate();
+              }}
+            />
+          </label>
+        </div>
+        <div>
+          <label>AWSD to move the camera.</label>
         </div>
       </div>
     );
